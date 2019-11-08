54d418179adf9770510637e395fb131569073ece
sakurai <kd1282334@st.kobedenshi.ac.jp>
2019-11-08 10:44:47 +0900

    11/8 課題

--- mypj/hello/views.py
+++ mypj/hello/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import HelloForm
 
 # Create your views here.
-def index(request, id, name):
-    result = 'あなたのIDは' + str(id) + '名前は' + name
+def index(request):
+    params = {
+        'title':'Hello/Index',
+        'msg':'あなたの情報:',
+        'form':HelloForm()
+    }
+    return render(request,'hello/index.html',params)
 
-    return HttpResponse(result)
+def next(request):
+    params = {
+        'title':'Hello/Next',
+        'msg':'これは,nextページです。',
+        'goto':'index',
+    }
+    return render(request,'hello/index.html',params)
+
+def form(request):
+    msg = request.POST['msg']
+    params = {
+    'title':'Hello/Form',
+    'msg':'こんにちは'+msg+'さん!',
+    }
+    if (request.method == 'POST'):
+        params['message'] = '名前:'+request.POST['name']+¥
+        '<br>メール:'+request.POST['mail']+¥
+        '<br>年齢:'+request.POST['age']
+        params['form'] = HelloForm(request.POST)
+    return render(request, 'hello/index.html', params)
